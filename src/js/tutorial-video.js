f67a35390a439cff7bb4443b3c4efab494249b7e
Joshua Schnee <joshschnee@ymail.com>
2022-10-01 12:53:33 -0500

    Add ui and javascript for quiz section and answer buttons

--- src/js/tutorial-video.js
+++ src/js/tutorial-video.js
@@ -3,21 +3,7 @@ function pause() {
   video.contentWindow.postMessage('{"event":"command", "func":"pauseVideo", "args":""}', '*');
 }
 let video = document.getElementById("tutorial-video-1")
-tutorialBox = document.querySelector(".tutorial-box");
-
-// tutorialBoxes.forEach(tutorialBox => {
-//   tutorialBox.addEventListener("mouseout", function(){
-//   // getting every iframe from the body
-//   var iframes = document.querySelectorAll('iframe');
-//   // reinitializing the values of the src attribute of every iframe to stop the YouTube video.
-//   for (let i = 0; i < iframes.length; i++) {
-//     if (iframes[i] !== null) {
-//       var temp = iframes[i].src;
-//       iframes[i].src = temp;
-//     }
-//   }
-// })
-// });
+tutorialBox = document.querySelector(".tutorial-box")
 
 tutorialBox.addEventListener('mouseout', (event) => {
   // pause();
