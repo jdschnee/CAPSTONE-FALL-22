06d947b46fd3f0a8deca542a6fcbfd69e9681ddf
Colin McGrath <cmcgrath454@gmail.com>
2022-10-10 19:02:56 -0500

    refactored visitor method and added while loop

--- src/js/analyze.js
+++ src/js/analyze.js
@@ -1,52 +1,22 @@
-import { getStatements } from './get-statements.js';
-import { parse } from "java-parser";
+import { parseCodeToTree } from "./parse-code.js";
 
 const javaText = `
 public class HelloWorldExample{
   public static void main(String args[]){
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {}
+    while (x < n) {
+      if (x < n) {
+        break;
+      }
     }
-    for (int k = 0; k < n; k++) {
-      for (int l = 0; l < n; l++) {}
-    }
+    for (int i = 0; i < n; i++) {}
   }
 }
 `;
 
-
-
 function getSourceCodeBigO(javaCode) {
-  const cst = parse(javaCode);
-  const stmtTree = buildStmtTree(cst);
+  const stmtTree = parseCodeToTree(javaCode);
 
   console.log(stmtTree);
 }
 
-function buildStmtTree(cst, parent = null) {
-    const stmts = getStatements(cst);
-    if (stmts.length == 0) return null;
-  
-    stmts.forEach((stmt, index) => {
-      stmts[index]['parent'] = parent;
-      stmts[index]['childStmts'] = [];
-
-      const childStmt = stmt.hasOwnProperty('blockCst') ? buildStmtTree(stmt.blockCst, stmt) : null;
-      if (stmt.hasOwnProperty('blockCst')) {
-        buildStmtTree(stmt.blockCst, stmt);
-        if (childStmt != null && childStmt.length > 1) throw ('Too many items in child statement array'); // TODO: For dev purposes only, remove later
-        if (childStmt != null) stmts[index]['childStmts'].push(childStmt[0]);
-      }
-    })
-
-    return stmts;
-}
-
 getSourceCodeBigO(javaText);
-
-
-
-
-
-
-
--- src/js/get-for-loops.js
+++ src/js/get-for-loops.js
@@ -1,4 +1,5 @@
 import { BaseJavaCstVisitorWithDefaults } from "java-parser";
+import { getLeafNodes } from "./util.js";
 
 class ForLoopCollector extends BaseJavaCstVisitorWithDefaults {
     constructor() {
@@ -10,35 +11,12 @@ class ForLoopCollector extends BaseJavaCstVisitorWithDefaults {
     basicForStatement(ctx) {
         const forLoop = {
             type: 'basicForLoop',
-            init: ctx.hasOwnProperty('forInit') ? this.getElements(ctx.forInit[0].children) : null,
-            expr: ctx.hasOwnProperty('expression') ? this.getElements(ctx.expression[0].children) : null,
-            update: ctx.hasOwnProperty('forUpdate') ? this.getElements(ctx.forUpdate[0].children) : null,
+            init: ctx.hasOwnProperty('forInit') ? getLeafNodes(ctx.forInit[0].children) : null,
+            expr: ctx.hasOwnProperty('expression') ? getLeafNodes(ctx.expression[0].children) : null,
+            update: ctx.hasOwnProperty('forUpdate') ? getLeafNodes(ctx.forUpdate[0].children) : null,
             blockCst: ctx.hasOwnProperty('statement') ? ctx.statement[0] : null
         };
         this.loops.push(forLoop);
-    }
-
-
-    getElements(ctx) {
-        const nodes = [];
-        this.getLeafNodes(ctx, nodes);
-        return nodes;
-    }
-
-    getLeafNodes(ctx, arr) {
-        const props = Object.getOwnPropertyNames(ctx);
-        props.forEach(prop => {
-            ctx[prop].forEach(elem => {
-                const childCtx = elem.children;
-                if (childCtx != null) {
-                    this.getLeafNodes(childCtx, arr);
-                } else {
-                    arr.push({
-                        [prop]: elem.image
-                    });
-                }
-            });
-        })
     }
 }
 
