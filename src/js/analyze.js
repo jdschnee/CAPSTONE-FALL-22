2f5e678b6eb7ec0cf30e354867907c9dfb9b5c5d
Colin McGrath <cmcgrath454@gmail.com>
2022-10-18 18:07:26 -0500

    Got Big-O for very basic for loops working

--- src/js/analyze.js
+++ src/js/analyze.js
@@ -1,3 +1,4 @@
+import { getForLoopBigO } from "./for-loops.js";
 import { parseCodeToTree } from "./parse-code.js";
 import { mapTree } from "./util.js";
 
@@ -5,12 +6,13 @@ import { mapTree } from "./util.js";
 const javaText = `
 public class HelloWorldExample{
   public static void main(String args[]){
-    while (x < n) {
-      if (x < n) {
-        break;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        for (int k = 0; k < n; k++) {}
       }
     }
-    for (int i = 0; i < n; i++) {}
+
+    for (int x = 0; x < n; x++) {}
   }
 }
 `;
@@ -19,10 +21,44 @@ function getSourceCodeBigO(javaCode) {
   const stmtTree = parseCodeToTree(javaCode);
 
   mapTree(stmtTree, addStmtBigO);
+
+  const res = findLargestBigO(stmtTree);
+  console.log(res);
+}
+
+function findLargestBigO(tree) {
+  mapTree(tree, bigOs);
+  return Math.max(...bigOList);
+}
+var bigOList = [];
+function bigOs(stmt) {
+  bigOList.push(stmt.bigO);
 }
 
 function addStmtBigO(stmt) {
-  // TODO: Add logic
+  let bigO = 0;
+  switch (stmt.type) {
+    case 'forLoop':
+      bigO = getForLoopBigO(stmt);
+      break;
+    case 'whileLoop':
+      bigO = 0;
+      // TODO: Add BigO Function
+      break;
+    case 'ifStmt':
+      bigO = 0;
+      // TODO: Add BigO Function
+      break;
+    default:
+      bigO = 0;
+      // TODO: Throw error?
+      break;
+  }
+  if (stmt.parent != null) {
+    stmt.bigO = stmt.parent.bigO + bigO;
+  } else {
+    stmt.bigO = bigO;
+  }
 }
 
 getSourceCodeBigO(javaText);
