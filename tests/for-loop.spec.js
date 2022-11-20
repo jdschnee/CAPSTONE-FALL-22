dc3b69d46ea251c228dec1e244fdae6e863bd0e8
Joshua Schnee <joshschnee@ymail.com>
2022-11-20 13:58:36 -0600

    Add testing for 4 nested for-loops

--- tests/for-loop.spec.js
+++ tests/for-loop.spec.js
@@ -91,3 +91,31 @@ test('3 Nested', async ({ page }) => {
   console.log()
   expect(answerTxt).toBe('O(N4)');
 });
+
+test('4 Nested', async ({ page }) => {
+  await page.goto('https://master.d3lx0rshiposis.amplifyapp.com/');
+  const codeEditor = await page.locator('textarea#editor.code-editor');
+  await codeEditor.clear();
+  const codeSnippet = `for(int i = 0; i < n; i++){	
+  for(int j = 0; j < n; j++){
+    for(int k = 0; k < n; k++){
+      for(int l = 0; l < n; l++){
+        for(int m = 0; m < n; m++){	
+        }	
+      }	
+    }	
+  }
+}`;
+  await codeEditor.type(codeSnippet)
+
+  await page.locator('#calculate-btn').click();
+  await page.locator('#calculator-section').getByRole('paragraph').filter({ hasText: 'A' }).click();
+  await page.locator('#calculator-section').getByText('B').click();
+  await page.locator('#calculator-section').getByRole('paragraph').filter({ hasText: 'C' }).click();
+  await page.locator('#calculator-section').getByText('D').click();
+
+  const answer = await page.locator('p[value="true"]');
+  const answerTxt = await answer.innerText();
+  console.log()
+  expect(answerTxt).toBe('O(N5)');
+});
