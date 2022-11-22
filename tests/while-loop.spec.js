24b8bcbff53849df7887e79f39c84944c25cbc03
Joshua Schnee <joshschnee@ymail.com>
2022-11-22 09:39:09 -0600

    Add testing for 1 while-loop

---
+++ tests/while-loop.spec.js
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test('0 Nested', async ({ page }) => {
+  await page.goto('https://master.d3lx0rshiposis.amplifyapp.com/');
+
+  const codeEditor = await page.locator('textarea#editor.code-editor');
+  await codeEditor.clear();
+  const codeSnippet = `int x = 0;
+while(x < n){	
+  x++;
+}`;
+  await codeEditor.type(codeSnippet)
+  // const codeSnippet = await codeEditor.inputValue();
+
+  await page.locator('#calculate-btn').click();
+  await page.locator('#calculator-section').getByRole('paragraph').filter({ hasText: 'A' }).click();
+  await page.locator('#calculator-section').getByText('B').click();
+  await page.locator('#calculator-section').getByRole('paragraph').filter({ hasText: 'C' }).click();
+  await page.locator('#calculator-section').getByText('D').click();
+
+  const answer = await page.locator('p[value="true"]');
+  const answerTxt = await answer.innerText();
+  expect(answerTxt).toBe('O(N)');
+});
