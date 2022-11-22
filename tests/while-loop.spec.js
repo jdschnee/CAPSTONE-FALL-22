8681c9c3c839ab76c2a077ae23dea86ceb28edda
Joshua Schnee <joshschnee@ymail.com>
2022-11-22 10:18:16 -0600

    Add testing for 1 nested while-loop

--- tests/while-loop.spec.js
+++ tests/while-loop.spec.js
@@ -10,7 +10,6 @@ while(x < n){
   x++;
 }`;
   await codeEditor.type(codeSnippet)
-  // const codeSnippet = await codeEditor.inputValue();
 
   await page.locator('#calculate-btn').click();
   await page.locator('#calculator-section').getByRole('paragraph').filter({ hasText: 'A' }).click();
@@ -21,4 +20,30 @@ while(x < n){
   const answer = await page.locator('p[value="true"]');
   const answerTxt = await answer.innerText();
   expect(answerTxt).toBe('O(N)');
+});
+
+test('1 Nested', async ({ page }) => {
+  await page.goto('https://master.d3lx0rshiposis.amplifyapp.com/');
+
+  const codeEditor = await page.locator('textarea#editor.code-editor');
+  await codeEditor.clear();
+  const codeSnippet = `int x = 0;
+while(x < n){	
+  x++;
+  int i;
+  while(i < n){
+    i++;
+  }
+}`;
+  await codeEditor.type(codeSnippet)
+
+  await page.locator('#calculate-btn').click();
+  await page.locator('#calculator-section').getByRole('paragraph').filter({ hasText: 'A' }).click();
+  await page.locator('#calculator-section').getByText('B').click();
+  await page.locator('#calculator-section').getByRole('paragraph').filter({ hasText: 'C' }).click();
+  await page.locator('#calculator-section').getByText('D').click();
+
+  const answer = await page.locator('p[value="true"]');
+  const answerTxt = await answer.innerText();
+  expect(answerTxt).toBe('O(N2)');
 });
